de4aceb7069e84e53a194fd2d1b3f909e58889bb
navitacion <>
2020-09-09 12:58:33 +0900

    課題1,2の修正反映

--- Submit/1_Python/task_3.py
+++ Submit/1_Python/task_3.py
@@ -7,7 +7,7 @@ even = [] # 偶数
 odd = []  # 奇数
 
 # 以下よりコードを記入してください  ##############################
-for i in range(6):
+for i in range(len(l)):
     if len(l[i]) %2 == 0:
         even.append(l[i])
     else:
